4bb4a939b0962c184e2980877485abd7370d477a
vishalsingh2710 <rvishal2710@gmail.com>
2023-07-29 18:51:28 +0000

    initial message

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+        alt="cart"
+        className="cart-img"
+      />
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
